45c5d8225c8e222cec52de4810b1df3b87915517
o.tsuyoshi823 <o.tsuyoshi823@gmail.com>
2019-01-29 14:15:09 +0900

    Initial Commit

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by tsuyoshi on 2019/01/29.
+//  Copyright © 2019 tsuyoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
